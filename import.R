3ecb6e5500bfcaab8c69f3e8e0761f6a641e6df1
David Lawrence Miller <github@ninepointeightone.net>
2020-02-13 14:28:15 +0000

    add Beaufort to segments

--- import.R
+++ import.R
@@ -5,6 +5,8 @@
 #   bugs added by Rexstad
 
 library(rgdal)
+library(lubridate)
+options(stringsAsFactors = FALSE)
 
 # base path
 base_path <- "rawdata/"
@@ -20,6 +22,7 @@ segs$x <- segs$POINT_X
 segs$y <- segs$POINT_Y
 segs$Effort <- segs$Length
 segs$Sample.Label <- segs$SegmentID
+segs$CenterTime <- ymd_hms(segs$CenterTime)
 
 # get rid of nuisance columns
 segs$Length <- segs$coords.x1 <- segs$coords.x2 <-
@@ -35,6 +38,70 @@ writeOGR(segs_shp, paste0(export_path, "segments.shp"), "data",
 dd <- unique(segs_shp@data[,c("SegmentID", "FIRST_Survey")])
 names(dd) <- c("Sample.Label", "Survey")
 segs <- merge(segs, dd, by="Sample.Label")
+
+# add in the Beaufort
+
+endat <- as.data.frame(readOGR(paste0(base_path, "Analysis.gdb"),"EN_Trackline2"))
+# make a time window
+endat$en04_effort_csv_begdatetime <- ymd_hms(endat$en04_effort_csv_begdatetime)
+endat$en04_effort_csv_enddatetime <- ymd_hms(endat$en04_effort_csv_enddatetime)
+endat$window <- endat$en04_effort_csv_begdatetime %--% endat$en04_effort_csv_enddatetime
+
+# get the EN survey
+segsEN <- subset(segs, Survey=="en04395")
+# make a beaufort column
+segsEN$Beaufort <- NA
+
+# loop over the segments
+for(j in 1:nrow(segsEN)){
+  # which interval does this segment lie in?
+  ind <- which(segsEN$CenterTime[j] %within% endat$window)
+  if(length(ind)==1){
+    segsEN$Beaufort[j] <- endat$en04_effort_csv_beaufort[ind]
+  }
+}
+
+# some were missing, interpolate
+naind <- is.na(segsEN$Beaufort)
+napred <- approx(as.numeric(segsEN$CenterTime)[!naind], segsEN$Beaufort[!naind],
+                 as.numeric(segsEN$CenterTime)[naind])
+
+# check plot
+#plot(segsEN$CenterTime[!naind], segsEN$Beaufort[!naind], pch=19)
+#points(as.numeric(segsEN$CenterTime)[naind], napred$y, pch=21)
+
+#now for GU
+gudat <- as.data.frame(readOGR(paste0(base_path, "Analysis.gdb"),"GU_Trackline2"))
+# make a time window
+gudat$DateTime1 <- ymd_hms(gudat$DateTime1)
+gudat$DateTime2 <- ymd_hms(gudat$DateTime2)
+gudat$window <- gudat$DateTime1 %--% gudat$DateTime2
+# get the EN survey
+segsGU <- subset(segs, Survey=="GU0403")
+# make a beaufort column
+segsGU$Beaufort <- NA
+
+# loop over the segments
+for(j in 1:nrow(segsGU)){
+  # which interval does this segment lie in?
+  ind <- which(segsGU$CenterTime[j] %within% gudat$window)
+  if(length(ind)==1){
+    segsGU$Beaufort[j] <- gudat$SeaState[ind]
+  }
+}
+
+# some were missing, interpolate
+naind <- is.na(segsGU$Beaufort)
+napred <- approx(as.numeric(segsGU$CenterTime)[!naind], segsGU$Beaufort[!naind],
+                 as.numeric(segsGU$CenterTime)[naind], method="constant")
+
+# check plot
+#plot(segsGU$CenterTime[!naind], segsGU$Beaufort[!naind], pch=19)
+#points(as.numeric(segsGU$CenterTime)[naind], napred$y, pch=21)
+
+
+# smoosh them back together again
+segs <- rbind(segsEN, segsGU)
 
 # write the csv
 write.csv(segs, file=paste0(export_path, "segments.csv"), row.names=FALSE)
@@ -57,7 +124,7 @@ obs$size <- obs$GroupSize
 obs$observer <- obs$detected <- 1
 
 # get rid of nuisance columns
-obs$Distance <- obs$SightingID <- obs$SegmentID <- obs$GroupSize <- segs$coords.x1 <- segs$coords.x2 <- NULL
+obs$Distance <- obs$SightingID <- obs$SegmentID <- obs$GroupSize <- NULL
 write.csv(obs, file=paste0(export_path, "obs.csv"), row.names=FALSE)
 
 # distance data
@@ -76,10 +143,10 @@ library(raster)
 # build a predictor stack
 predictorStack <- stack(paste0(base_path,
                         c("Covariates_for_Study_Area/Depth.img",
-                          "Covariates_for_Study_Area/GLOB/CMC/CMC0.2deg/analysed_sst/2004/20040602-CMC-L4SSTfnd-GLOB-v02-fv02.0-CMC0.2deg-analysed_sst.img",
+                          "Covariates_for_Study_Area/GLOB/CMC/CMC0.2deg/analysed_sst/2004/20040624-CMC-L4SSTfnd-GLOB-v02-fv02.0-CMC0.2deg-analysed_sst.img",
                           "Covariates_for_Study_Area/VGPM/Rasters/vgpm.2004153.hdf.gz.img",
                           "Covariates_for_Study_Area/DistToCanyonsAndSeamounts.img",
-                          "Covariates_for_Study_Area/Global/DT\ all\ sat/MSLA_ke/2004/MSLA_ke_2004153.img"
+                          "Covariates_for_Study_Area/Global/DT\ all\ sat/MSLA_ke/2004/MSLA_ke_2004176.img"
                           )))
 # rename the layers in our stack to match those in the model 
 names(predictorStack) <- c("Depth","SST","NPP", "DistToCAS", "EKE")
